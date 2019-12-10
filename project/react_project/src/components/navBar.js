2afb4fd1db0e16b3cd64ece749e0634cf0309e21
BuildTools <unconfigured@null.spigotmc.org>
2019-12-10 07:20:00 +0200

    links now work with pages

--- project/react_project/src/components/navBar.js
+++ project/react_project/src/components/navBar.js
@@ -1,21 +1,26 @@
 import React, {useState} from 'react';
 import '../navBar.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="navBar">
-      
+        <Link className="linkStyle" to='/feed'>
         <ul className="navBarContent">
           Feed
         </ul>
+        </Link>
+        <Link className="linkStyle" to='/profile'>
         <ul className="navBarContent">
           Profile
         </ul>
+        </Link>
+        <Link className="linkStyle" to='/settings'>
         <ul className="navBarContent">
           Settings
         </ul>
+        </Link>
       </div>
   )
 }
