41ff0a3902ad613fb7d97be7af319c19368c3b5d
BuildTools <unconfigured@null.spigotmc.org>
2019-12-10 18:46:55 +0200

    Express project updated to use rahti

--- project/expressProject/index.js
+++ project/expressProject/index.js
@@ -16,55 +16,40 @@ app.use('/posts', postsRoute);
 
 
 
-var mongoURL = "mongodb://localhost:27017/expressDatabase";
+// var mongoURL = "mongodb://localhost:27017/expressDatabase";
 
-mongoose.connect(mongoURL, { useNewUrlParser: true }, () => 
-console.log("Database connection established!"))
+// mongoose.connect(mongoURL, { useNewUrlParser: true }, () => 
+// console.log("Database connection established!"))
 
 app.listen(3000, () => {
     console.log("started database server")
 });
 
-// FOR RAHTI ONLY *******************************************************************
-// var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
-// var mongoURLLabel = "";
+// Next lines are for rahti deployed database structure:
+// Not my own code, directly copied and edited from the course demo.
+const databaseHook = require('./db');
+const mongoURL = databaseHook.urlbuilder();
 
-// For local dev
-// var mongoURL = 'mongodb://localhost:27017/demodb';
+databaseHook.connect(databaseHook.urlbuilder(), function(err) {
+if (err) {
+    console.log("Unable to connect to Mongo.");
+    process.exit(1);
+}
+});
 
-// if (mongoURL == null) {
-//     var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-//     // If using plane old env vars via service discovery
-//     if (process.env.DATABASE_SERVICE_NAME) {
-//         var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-//         mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
-//         mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
-//         mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
-//         mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
-//         mongoUser = process.env[mongoServiceName + "_USER"];
+// Catch 404 and forward to error handler - default
+app.use(function(req, res, next) {
+next(createError(404));
+});
 
-//         // If using env vars from secret from service binding
-//     } else if (process.env.database_name) {
-//         mongoDatabase = process.env.database_name;
-//         mongoPassword = process.env.password;
-//         mongoUser = process.env.username;
-//         var mongoUriParts = process.env.uri && process.env.uri.split("//");
-//         if (mongoUriParts.length == 2) {
-//             mongoUriParts = mongoUriParts[1].split(":");
-//             if (mongoUriParts && mongoUriParts.length == 2) {
-//                 mongoHost = mongoUriParts[0];
-//                 mongoPort = mongoUriParts[1];
-//             }
-//         }
-//     }
+// Register error handler - default
+app.use(function(err, req, res, next) {
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get("env") === "development" ? err : {};
 
-//     if (mongoHost && mongoPort && mongoDatabase) {
-//         mongoURLLabel = mongoURL = "mongodb://";
-//         if (mongoUser && mongoPassword) {
-//         mongoURL += mongoUser + ":" + mongoPassword + "@";
-//         }
-//         // Provide UI label that excludes user id and pw
-//         mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
-//         mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
-//     }
-// }
+// render the error page
+res.status(err.status || 500);
+res.render("error");
+});
+  
